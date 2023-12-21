047b9a1c8bf6d3af5abba48c8d3c6950e4cd47db
Christopher Panerio <christopherpaneriotup@gmail.com>
2023-12-21 14:18:50 +0800

    Frontend Updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ALFC Design Phase</title>
+    <title>Alfc Insurance Agency Corporation.</title>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,11 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('Auth/login');
 });
+
+Route::get('/Sales-Associate', function () {
+    return view('Users/Sales_Associate/Home/homepage');
+});
+
+Route::get('/Sales-Associate/Marketing-Arms/', function () {
+    return view('Users/Sales_Associate/ApplicationForms/Index');
+});

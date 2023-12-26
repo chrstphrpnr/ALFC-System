65850d22c30c12a80039ed8d9278b39dd24c1ffb
Christopher Panerio <christopherpaneriotup@gmail.com>
2023-12-26 10:13:47 +0800

    Added Debug Bar

--- config/app.php
+++ config/app.php
@@ -194,7 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
@@ -211,5 +211,8 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
+
+    'Debugbar' => Barryvdh\Debugbar\Facade::class,
+
 
 ];

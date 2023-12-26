dba41887c4144d50df08dd01d5ae34688cf53639
Christopher Panerio <christopherpaneriotup@gmail.com>
2023-12-26 10:49:19 +0800

    Provider Products Models Migration

---
+++ app/Models/ProviderProducts.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProviderProducts extends Model
+{
+    use HasFactory;
+    public $table = 'provider_products';
+
+    protected $fillable = [
+        'insurance_provider_id',
+        'insurance_product_id',
+        'insurance_product_type'
+    ];
+
+    public $timestamp = false;
+    public $primaryKey = 'id';
+}
